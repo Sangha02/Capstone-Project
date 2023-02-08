dc5d9399119210af86a68ece9af80067e5558dd4
Sanghamitra Dutta <iradutta02@gmail.com>
2023-02-08 13:49:12 +0530

    second commit

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import {SubHeading} from '../../components';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin:'2rem 0' }}>The true mark of professionalism is the ability to respect everyone else for their styles and always find something positive in every dining experience and highlight it in your thoughts and words.</p>
+      <button type="buton" className="custom__button">Explore Menu</button>   
+    </div>
+    <div className="app__wrapper_img">
+
+    </div>
   </div>
 );
 
